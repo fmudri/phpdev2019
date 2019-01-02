68cbc8b28833e97c2a8c801924850ba32767299f
fmudri <filipmudri3@gmail.com>
2019-01-02 21:54:23 +0100

    sql

--- fmudri/mysql_vjezbe/studenti.sql
+++ fmudri/mysql_vjezbe/studenti.sql
@@ -3,9 +3,11 @@ student.id,
 student.student,
 student.prosjek,
 predmet.predmet
-FROM 
-fakultet.student INNER JOIN ispit ON ispit.student_id=student.id 
-INNER JOIN fakultet.predmet ON predmet.id=ispit.id 
+FROM fakultet.student 
+INNER JOIN ispit 
+ON ispit.student_id=student.id 
+INNER JOIN fakultet.predmet 
+ON predmet.id=ispit.id 
 WHERE prosjek>2.5
 GROUP BY student.prosjek
 HAVING prosjek<4.5
